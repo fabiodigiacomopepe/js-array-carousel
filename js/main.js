c53937a7273566d74e6330ac3a738d71c8ec5a0d
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-05-04 18:38:45 +0200

    Completed milestone 3

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const alt = ["foto1", "foto2", "foto3", "foto4", "foto5"];
 
 // Creo loop
 for (let i = 0; i < immagini.length; i++) {
-    let divItem = `<div class="item ${[i + 1]}">`;              // Creo DIV
+    let divItem = `<div class="item ${[i]}">`;                  // Creo DIV
     let img = `<img src="${immagini[i]}" alt="${alt[i]}">`;     // Creo IMMAGINe
     console.log(divItem);       // Riporto stringa DIV generata in console
     console.log(img);           // Riporto stringa IMG generata in console
@@ -20,3 +20,25 @@ primoItem.classList.add("active");
 // Loggo tutto il risultato in console
 const items = document.querySelectorAll('.item');
 console.log(items);
+
+// Setto item ATTIVO al momento
+let itemAttivo = 0;
+
+// Seleziono pulsante NEXT
+const next = document.querySelector(".next");
+
+// Collego funzione ad EVENTO su NEXT
+next.addEventListener("click", miaFunzione);
+
+function miaFunzione() {
+    if (itemAttivo < (items.length - 1)) {              // SE sono nelle prime 4 immagini
+        items[itemAttivo].classList.remove("active");   // Rimuovo classe ACTIVE all'ITEM attualmente attivo
+        itemAttivo += 1;                                // Incremento valore dell' ITEM ATTIVO
+        items[itemAttivo].classList.add("active");      // Assegno classe ACTIVE all'elemento (attualmente) successivo
+        
+        if (itemAttivo === (items.length -1)) {         // SE sono nell'ultima foto
+            next.classList.add("hidden");               // NASCONDO pulsante NEXT
+        }
+    }
+
+}
