e1a7223a71b4604e3025009fa93b36318afed61d
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-05-07 19:20:10 +0200

    Add side slider

--- js/main.js
+++ js/main.js
@@ -4,22 +4,41 @@ const alt = ["foto1", "foto2", "foto3", "foto4", "foto5"];
 
 // Creo loop
 for (let i = 0; i < immagini.length; i++) {
+    // SLIDER PRINCIPALE
     let divItem = `<div class="item ${[i]}">`;                  // Creo DIV
-    let img = `<img src="${immagini[i]}" alt="${alt[i]}">`;     // Creo IMMAGINe
+    let img = `<img src="${immagini[i]}" alt="${alt[i]}">`;     // Creo IMMAGINE
     console.log(divItem);       // Riporto stringa DIV generata in console
     console.log(img);           // Riporto stringa IMG generata in console
 
     // Innesto DIV con all'interno IMG nell'ITEMS-CONTAINER (Template Literal)
     document.querySelector('.items-container').innerHTML += `${divItem}${img}`;
+
+
+
+    // SLIDER LATERALE
+    let divCard = `<div class="card">`;                                              // Creo DIV
+    let imgCard = `<div class="layer"></div>                                        
+                   <img class="img-card" src="${immagini[i]}" alt="${alt[i]}">`;     // Creo LAYER E IMMAGINE
+    console.log(divCard);       // Riporto stringa DIV generata in console
+    console.log(imgCard);       // Riporto stringa IMG generata in console
+
+    // Innesto DIV con all'interno IMG nell'ITEMS-CONTAINER (Template Literal)
+    document.querySelector('.slider-laterale').innerHTML += `${divCard}${imgCard}`;
 }
 
 // Seleziono primo ITEM creato e aggiungo classe ACTIVE
 let primoItem = document.querySelector('.item');
 primoItem.classList.add("active");
 
-// Loggo tutto il risultato in console
+// Seleziono primo LAYER creato e aggiungo classe HIDDEN
+let primoLayer = document.querySelector('.layer');
+primoLayer.classList.add("hidden");
+
+// Collego costante ITEMS a elenco ITEM
 const items = document.querySelectorAll('.item');
-console.log(items);
+
+// Collego costante LAYERS a elenco LAYER
+const layers = document.querySelectorAll('.layer');
 
 // Setto item ATTIVO al momento
 let itemAttivo = 0;
@@ -35,11 +54,16 @@ next.addEventListener("click", miaFunzione);
 
 function miaFunzione() {
     items[itemAttivo].classList.remove("active");   // Rimuovo classe ACTIVE all'ITEM attualmente attivo
+
+    layers[itemAttivo].classList.remove("hidden");  // Rimuovo classe HIDDEN al LAYER attualmente attivo
+
     if (itemAttivo === (items.length - 1)) {        // SE mi trovo nell'ULTIMA foto
         itemAttivo = -1;                            // Setto valore ITEM ATTIVO a -1 (così incrementato diventa 0)
     }
     itemAttivo = itemAttivo + 1;                    // Incremento valore dell' ITEM ATTIVO
     items[itemAttivo].classList.add("active");      // Assegno classe ACTIVE all'elemento (attualmente) successivo
+
+    layers[itemAttivo].classList.add("hidden");     // Assegno classe HIDDEN all'elemento (attualmente) successivo
 }
 
 // Collego funzione ad EVENTO su PREVIOUS
@@ -47,9 +71,14 @@ previous.addEventListener("click", miaFunzione1);
 
 function miaFunzione1() {
     items[itemAttivo].classList.remove("active");   // Rimuovo classe ACTIVE all'ITEM attualmente attivo
+
+    layers[itemAttivo].classList.remove("hidden");  // Rimuovo classe HIDDEN al LAYER attualmente attivo
+
     if (itemAttivo === 0) {                         // SE mi trovo nella PRIMA foto
         itemAttivo = items.length;                  // Setto valore a ITEMS.LENGTH (5) (così decrementato diventa 4, cioè ultima foto)
     }
     itemAttivo = itemAttivo - 1;                    // Decremento valore dell' ITEM ATTIVO
     items[itemAttivo].classList.add("active");      // Assegno classe ACTIVE all'elemento (attualmente) successivo
+
+    layers[itemAttivo].classList.add("hidden");     // Assegno classe HIDDEN all'elemento (attualmente) successivo
 }
